29b6f7aab30ee544a0cb5dbf3b1c8f42c9dec2d0
nakhoa <nakhoa17@gmail.com>
2022-03-04 14:48:44 +0700

    chore: minor change

--- src/components/kits/tabs/index.vue
+++ src/components/kits/tabs/index.vue
@@ -6,7 +6,7 @@
           v-for="(tab, idx) in tabs"
           :key="idx"
           :class="[
-            true
+            current.key === tab.key
               ? 'bg-gray-100 text-gray-700'
               : 'text-gray-500 hover:text-gray-700',
             'px-3 py-2 font-medium text-sm rounded-md cursor-pointer',
@@ -38,7 +38,7 @@ export default {
   },
 
   setup(props, { emit }) {
-    const currentComputed = computed({
+    const current = computed({
       get: () => {
         return props.modelValue;
       },
@@ -48,13 +48,12 @@ export default {
     });
 
     const select = (tab) => {
-      console.log({ tab });
-      currentComputed.value = tab;
+      current.value = tab;
     };
 
     return {
       select,
-      currentComputed,
+      current,
     };
   },
 };
