9bdcd4b9287a86c6edd362dc79e149d9cdea645c
kunguyenqt <lehoangbinhnguyen@gmail.com>
2022-03-05 21:24:51 +0700

    add table company info

--- src/components/symbol-detail/tabs/company-detail/index.vue
+++ src/components/symbol-detail/tabs/company-detail/index.vue
@@ -1,9 +1,40 @@
 <template>
   <div class="flex">
-    <main class="flex-1 relative z-0 focus:outline-none">
-      <div class="inset-0 py-3 px-2">
-        <div>
+    <main class="flex-1 relative z-0 focus:outline-none overflow-y-scroll">
+      <div class="inset-0">
+        <div class="py-3 px-2">
           {{ profile.overview }}
+        </div>
+
+        <div class="px-3 py-3 bg-gray-200 shadow overflow-x-scroll">
+          <table class="min-w-full divide-y divide-gray-300">
+            <thead>
+            <tr>
+              <th  scope="col" colspan="2" class="px-3 py-3 text-left text-sm font-bold text-gray-900 uppercase tracking-wider">THÔNG TIN CƠ BẢN</th>
+              <th scope="col" colspan="2" class="px-3 py-3 text-left text-sm font-bold text-gray-900 uppercase tracking-wider">THÔNG TIN NIÊM YẾT</th>
+            </tr>
+            </thead>
+            <tbody class="bg-white divide-y divide-gray-200">
+            </tbody>
+
+            <tr
+                v-for="row in companyInfo"
+                :key="row.key"
+            >
+              <td class="px-3 py-3 whitespace-nowrap text-sm text-gray-800">
+                {{row[0].name}}
+              </td>
+              <td class="px-3 py-3 whitespace-nowrap text-sm text-right font-bold text-gray-900">
+                {{ profile[row[0].key] }}
+              </td>
+              <td class="px-3 py-3 whitespace-nowrap text-sm text-gray-800">
+                {{row[1].name}}
+              </td>
+              <td class="px-3 py-3 whitespace-nowrap text-sm text-right font-bold text-gray-900">
+                {{ profile[row[1].key] }}
+              </td>
+            </tr>
+          </table>
         </div>
       </div>
     </main>
@@ -36,6 +67,14 @@
 import axios from 'axios'
 import { ref } from 'vue'
 
+const companyInfo = [
+  [{name: 'Mã SIC', key: 'symbol'}, {name: 'Ngày niêm yết', key:'dateOfListing'}],
+  [{name: 'Mã ngành ICB', key: 'icbCode'}, {name: 'Nơi niêm yết', key:'exchange'}],
+  [{name: 'Năm thành lập', key: 'establishmentDate'}, {name: 'Giá chào sàn (VNĐ)', key:'initialListingPrice'}],
+  [{name: 'Vốn điều lệ', key: 'listingVolume'}, {name: 'Ngày phát hành cuối', key:'dateOfIssue'}],
+  [{name: 'Số lượng nhân sự', key: 'employees'}, {name: 'KL đang niêm yết', key:'charterCapital'}]
+]
+
 export default {
   name: 'CompanyDetail',
 
@@ -47,6 +86,9 @@ export default {
       .get('/api/symbols/FPT/profile')
       .then((res) => {
         profile.value = res.data
+        profile.value.dateOfListing = new Intl.DateTimeFormat('vi', {month:'2-digit',day:'2-digit', year:'numeric'}).format(new Date(profile.value.dateOfListing))
+        profile.value.establishmentDate = new Intl.DateTimeFormat('vi', {month:'2-digit',day:'2-digit', year:'numeric'}).format(new Date(profile.value.establishmentDate))
+        profile.value.dateOfIssue = new Intl.DateTimeFormat('vi', {month:'2-digit',day:'2-digit', year:'numeric'}).format(new Date(profile.value.dateOfIssue))
       })
       .catch((err) => {
         console.log(err)
@@ -70,6 +112,7 @@ export default {
     return {
       profile,
       officers,
+      companyInfo,
     }
   },
 }
