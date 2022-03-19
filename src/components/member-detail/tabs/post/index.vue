9c8e305d84e889c2fb067bc3f6f9bf674b80d6f0
ngo van nguyen <chickie8x@ngos-iMac-Pro.local>
2022-03-19 17:32:39 +0700

    fix post detail page due to API changed

---
+++ src/components/member-detail/tabs/post/index.vue
@@ -0,0 +1,121 @@
+<template>
+  <div v-if="post" class="flex flex-col">
+    <div class="sm:flex sm:items-start sticky top-0">
+      <div
+        class="text-center sm:text-left w-full bg-white rounded-t-md border-b-gray-100"
+      >
+        <div class="flex px-5 py-2">
+          <NavRouter />
+          <DialogTitle
+            as="h3"
+            class="text-lg leading-6 font-medium text-gray-900 flex text-center my-auto ml-4"
+            >Chi tiết</DialogTitle
+          >
+        </div>
+      </div>
+    </div>
+
+    <div class="w-full h-full bg-blue-50/[0.8] p-5">
+      <div class="flex border-b border-gray-300">
+        <div class="flex items-center w-full space-x-2 pb-2">
+          <Likes :fill="iconColor" />
+          <span class="text-[#106ba3] text-sm"
+            >Mickey và 1 người khác đã thích</span
+          >
+        </div>
+        <div class="m-1">
+          <button
+            type="button"
+            class="inline-flex items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-white bg-[#106ba3] hover:bg-[#0b517c] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#0b517c]"
+          >
+            <Likes :fill="btnColor" class="mr-1" />
+            Thích
+          </button>
+        </div>
+      </div>
+      <div class="text-3xl font-semibold py-3">{{ post.title }}</div>
+      <div class="flex items-center justify-between pb-3">
+        <div class="flex text-gray-500 text-sm divide-x space-x-2">
+          <div>{{ post.postSource.name }}</div>
+          <div class="text-sm text-gray-500">
+            {{ new Date(post.date).toLocaleDateString() }} lúc
+            {{ new Date(post.date).toLocaleTimeString() }}
+          </div>
+        </div>
+        <div class="space-x-1">
+          <button
+            type="button"
+            class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-[#106ba3] hover:bg-[#0b517c]/[0.2] focus:outline-none"
+          >
+            <Share :fill="iconColor" />
+            Chia sẻ
+          </button>
+
+          <button
+            type="button"
+            class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-[#106ba3] hover:bg-[#0b517c]/[0.2] focus:outline-none"
+          >
+            <Repost :fill="iconColor" />
+            Đăng lại
+          </button>
+
+          <button
+            type="button"
+            class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-[#106ba3] hover:bg-[#0b517c]/[0.2] focus:outline-none"
+          >
+            <Replies :fill="iconColor" class="mr-1" />
+            Bình luận
+          </button>
+        </div>
+      </div>
+      <div class="h-full overflow-auto">
+        <div v-html="post.content"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import NavRouter from '../../../kits/nav-router/index.vue'
+import { useRoute } from 'vue-router'
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+import Likes from '../../../svg/likes.vue'
+import Share from '../../../svg/share.vue'
+import Replies from '../../../svg/replies.vue'
+import Repost from '../../../svg/repost.vue'
+
+export default {
+  components: {
+    NavRouter,
+    Likes,
+    Share,
+    Replies,
+    Repost,
+  },
+  setup() {
+    // data
+    const route = useRoute()
+    const post = ref(null)
+    const postID = route.params.id
+    const iconColor = ref('#106ba3')
+    const btnColor = ref('white')
+
+    // methods
+    const fetchPost = async () => {
+      post.value = await (await axios.get(`/news/${postID}/`)).data
+    }
+    onMounted(() => {
+      fetchPost()
+    })
+
+    return {
+      post,
+      iconColor,
+      btnColor,
+    }
+  },
+}
+</script>
+
+<style></style>
--- src/views/entry/symbols/tabs/dividend/index.vue
+++ src/views/entry/symbols/tabs/dividend/index.vue
@@ -1,11 +1,105 @@
 <template>
-  <div class="border-2 border-gray-200 border-dashed rounded-lg h-96">
-    Dividend
+  <div class="border-2 border-gray-200 rounded-lg min-h-full flex">
+
+    <div class="w-2/3 h-full"> <!-- table wrapper -->
+      <div class="px-4 sm:px-6 lg:px-8">
+        <div class=" flex flex-col">
+          <div class="-my-2 -mx-4 sm:-mx-6 lg:-mx-8">
+            <div class="inline-block min-w-full py-2 align-middle">
+              <div class="shadow-sm ring-1 ring-black ring-opacity-5">
+                <table class="min-w-full border-separate" style="border-spacing: 0">
+                  <thead class="">
+                    <tr>
+                      <th
+                        v-for="header, idx in headers" :key="idx"
+                        scope="col"
+                        class="sticky top-0 z-10 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 lg:pl-8"
+                      >{{ header.text }}</th>
+                    </tr>
+                  </thead>
+                  <tbody class="bg-white">
+                    <tr 
+                      v-for="(event, evIdx) in events" :key="evIdx"
+                      :class="[evIdx%2===0?' bg-gray-100':'', 'cursor-pointer hover:bg-gray-200']"
+                     >
+                      <td
+                        :class="[, 'whitespace-nowrap py-4 pl-4 pr-3 text-sm text-gray-900 sm:pl-6 lg:pl-8']"
+                      >{{ $filters.formatDate(event.recordDate) }}</td>
+
+                       <td
+                        :class="[, 'whitespace-nowrap py-4 pl-4 pr-3 text-sm text-gray-900 sm:pl-6 lg:pl-8']"
+                      >{{ event.title }}</td>
+
+                       <td
+                        :class="[, 'whitespace-nowrap py-4 pl-4 pr-3 text-sm text-gray-900 sm:pl-6 lg:pl-8']"
+                      >{{ $filters.formatDate(event.executionDate) }}</td>
+
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="w-1/3 h-full bg-[#d1e5f1]"> <!-- charts wrapper -->
+      <div class="">
+
+      </div>
+
+      <div class="">
+
+      </div>
+    
+    </div>
+
   </div>
 </template>
 
 <script>
+
+import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+import axios from 'axios'
+
+const headers = [
+  { id: 1, prefix: 'GD', text: 'GD KHQ' },
+  { id: 2, prefix: 'SK', text: 'SỰ KIỆN' },
+  { id: 3, prefix: 'TH', text: 'THỰC HIỆN' },
+]
+
 export default {
   name: 'Dividend',
+
+  setup() {
+    const route = useRoute()
+    const chartData = ref([])
+    const events = ref([])
+    const symbol = route.params.symbol
+    const current_date = ref(new Date().toISOString().split('.')[0])
+    const offset = ref(0)
+
+    const fetchData = async () => {
+      events.value = await (
+        await axios.get(
+          `/events/${symbol}/?start_date=2019-03-11T04%3A54%3A28&end_date=${encodeURIComponent(
+            current_date.value,
+          )}&offset=${offset.value}&limit=20`,
+        )
+      ).data
+    }
+
+    onMounted(() => {
+      fetchData()
+    })
+
+    return {
+      chartData,
+      events,
+      headers
+    }
+  }
 }
 </script>
